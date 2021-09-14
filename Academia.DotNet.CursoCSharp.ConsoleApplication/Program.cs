2e636afe77801f14b9f75d6eac081dd1e3e1f922
Rondinelli Pereira <rondinelli.pereira@outlook.com>
2021-09-13 22:43:59 -0300

    Add project files.

---
+++ Academia.DotNet.CursoCSharp.ConsoleApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//Entendendo o que sao namespace: Forma que o .Net Framework utiliza para organizar os seus projeto.
+//Sao formas logicas de se separar o seu software.
+//Note que nesse projeto , a class Program está dentro do namespace AcademiaDontNet.CursoCSharp
+//No outro projeto que criamos, AcademiaDontNet.CursoCSharp2 também há a class Program, porém, está dentro do namespace AcademiaDontNet.CursoCSharp2.
+//Sendo assim, não há nenhum problema em trabalhar com ambos assim. 
+
+
+namespace AcademiaDotNet.CursoCSharp.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
---
+++ Academia.DotNet.CursoCSharp.ConsoleApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+//Referenciamento de um projeto para o outro: Através da palavra reservada "using", pode ser usado todos os recursos
+//do segundo projeto ConsoleApplication.
+using AcademiaDotNet.CursoCSharp.ConsoleApplication;
+
+namespace AcademiaDotNet.CursoCSharp.ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
